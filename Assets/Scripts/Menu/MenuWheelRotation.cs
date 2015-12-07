7c64e047d990d9f57a2f5fbacb95a876d0fa4619
Kevin Liu <Kevin Liu>
2015-12-07 00:53:54 -0500

    Add main menu

---
+++ Assets/Scripts/Menu/MenuWheelRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuWheelRotation : MonoBehaviour {
+
+	float rotationAmount = 720.0f;
+	
+	void Update() {
+		transform.Rotate(rotationAmount * Time.deltaTime, 0, 0);
+	}
+}
---
+++ Assets/Scripts/Menu/Start.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start : MonoBehaviour {
+
+	void OnMouseUp() {
+		Application.LoadLevel(1);
+	}
+}
