9dfb02349ea5280f9afdb343c46a64669346e8dd
KDorosh <kcdorosh@gmail.com>
2015-12-12 17:05:17 -0500

    changed back 2 player room name

---
+++ Assets/isAlive.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isAlive : MonoBehaviour {
+
+	public bool status = true;
+	
+	public void playerDied () {
+		status = false;
+	}
+	
+	public bool getPlayerStatus () {
+		return status;
+	}
+}
