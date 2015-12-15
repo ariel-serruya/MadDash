6e8b314f3a04d46ff410e40edef2d1dbb19aad80
Kevin Liu <Kevin Liu>
2015-12-15 02:46:50 -0500

    Fix font size on mobile and upload executables

--- Assets/Scripts/Menu/About.cs
+++ Assets/Scripts/Menu/About.cs
@@ -23,7 +23,7 @@ public class About : MonoBehaviour {
 				gui.fontSize = 24;
 				GUI.Label (new Rect (10, 10, Screen.width, Screen.height), "Controls: \n - Move: Arrow Keys or WASD \n - Shoot: P \n - Change Camera: V \n - Quit: ESC\n - Meteor Storm: O\n\n Collect 3 meteors to get Meteor Storm\n Collect wrenches to heal", gui);
 			#else
-				gui.fontSize = 24;
+				gui.fontSize = 76;
 				GUI.Label (new Rect (10, 10, Screen.width, Screen.height), "Controls: \n - Move: Joystick \n - Shoot: Shoot Button \n - Change Camera: Swipe Left/Right \n - Quit: Phone Default\n - Meteor Storm: Swipe down \n\n Collect 3 meteors to get Meteor Storm\n Collect wrenches to heal", gui);
 			#endif
 		} else {
